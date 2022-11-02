83647f1330ccffd0ddbc5256dd12dfdd13e4f3fa
KimKyungYun <60084235+KimKyungYun@users.noreply.github.com>
2022-11-02 13:51:54 +0900

    sign up JS

--- signup.js
+++ signup.js
@@ -1,6 +1,6 @@
 const id_check = new RegExp('^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$');
 const pass_check = new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$');
-const stnum_check = new RegExp('[0-9]{10}');
+const stnum_check = new RegExp('^20[0-2]{1}[0-9]{1}[0-9]{6}');
 const phone_check = new RegExp('\^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})');
 
 const id = document.querySelector('#InputID1');
